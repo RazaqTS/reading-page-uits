b64f89189b07f7f68cf46ea64d14400f66272090
Taiwo <t.razaq@elsevier.com>
2024-11-25 23:33:51 +0000

    add git action to run CI

---
+++ fixtures/hook.js
@@ -0,0 +1,20 @@
+// // hooks.js
+// import { test } from '@playwright/test';
+
+// test.beforeAll(async () => {
+//   console.log('Global setup: Runs once before all tests');
+// });
+
+// test.beforeEach(async ({ page }) => {
+//   console.log('Setting up before each test');
+//   await page.goto('https://example.com');
+// });
+
+// test.afterEach(async ({ page }) => {
+//   console.log('Cleaning up after each test');
+//   await page.close();
+// });
+
+// test.afterAll(async () => {
+//   console.log('Global teardown: Runs once after all tests');
+// });
