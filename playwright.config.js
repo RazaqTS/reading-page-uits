071e563968d5b9902a774aa9bcc78fbe061ea1a8
Taiwo <t.razaq@elsevier.com>
2024-11-26 00:04:15 +0000

    change env to prod

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@ const { devices } = require('@playwright/test');
 
 const config = {
   testDir: './tests',  // Directory where your test files are located
-  retries: 1,          // Number of retries on failure
+  retries: 2,          // Number of retries on failure
   use: {
     headless: true,    // Set to false if you want the browser to open with UI
     trace: 'on-first-retry',  // Enable trace on first retry
